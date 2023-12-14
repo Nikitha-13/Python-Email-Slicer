62d2afed3422cec985b8352e1d5fad0f0c893051
Nikitha Pindi <143797613+Nikitha-13@users.noreply.github.com>
2023-12-14 20:37:58 +0530

    Update emailslicer.py

--- emailslicer.py
+++ emailslicer.py
@@ -1,5 +1,12 @@
+# Code to take input(Email) from user
 email = input("Enter Your Email: ").strip()
+
+# Slicing the Email
 username = email[ :email.index("@")]
 domain_name = email[email.index("@")+1:]
+
+# Formatting the sliced Email in understandable way
 format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
+
+# Printing the output
 print(format_)
